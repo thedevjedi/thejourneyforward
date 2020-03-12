a2b7091a4ddfeb2c273b1b0e688d53e2f10b8e8a
Dev Jedi <thedevjedi@gmail.com>
2020-03-12 09:00:23 -0500

    added more changes

--- anotherone2.py
+++ anotherone2.py
@@ -10,3 +10,4 @@ print(check_zip_code("The zip codes for New York are 10001 thru 11104.")) # True
 print(check_zip_code("90210 is a TV show")) # False
 print(check_zip_code("Their address is: 123 Main Street, Anytown, AZ 85258-0001.")) # True
 print("this is an add test")
+print("this is another test")
